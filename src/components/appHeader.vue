9c3c3d2d3fc17b68dbaabbeb147a0c729269b6a6
IgorMMichels <igor.marcon.michels@gmail.com>
2026-05-11 16:57:41 -0300

    feat: Componentização header, criação de views e estilização inicial da base, adição da fonte customizada também

---
+++ src/components/appHeader.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <header>
+      <img src="/public/favicon.ico" alt="icon" />
+      <h1>Entrelinhas</h1>
+    <div></div>
+  </header>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+ import appHeader from '@/components/appHeader.vue';
+</script>
+
+<template>
+  <appHeader></appHeader>
+  <div>
+    <h2>BEM VINDO A LOJINHA BONITA</h2>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
