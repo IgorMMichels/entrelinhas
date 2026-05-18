389f618cab0c87ae0484bc6b3332b9358368f9c8
brunasiebertm <bruna.siebertm@gmail.com>
2026-05-18 11:59:40 -0300

    feat: criação product.js

---
+++ src/data/product.js
@@ -0,0 +1,68 @@
+const produtos = [
+  {
+    "id": 1,
+    "titulo": " Harry Potter e a Pedra Filosofal ",
+    "autor": "J.K. Rowling",
+    "resenha": "Um livro sobre boas práticas de programação, escrita de código limpo e melhoria da qualidade de software.",
+    "preco": 69.90,
+    "capa": "https://covers.openlibrary.org/b/isbn/9780132350884-L.jpg"
+  },
+  {
+    "id": 2,
+    "titulo": "Harry Potter e a Câmara Secreta",
+    "autor": "J.K. Rowling",
+    "resenha": "Um livro sobre boas práticas de programação, escrita de código limpo e melhoria da qualidade de software.",
+    "preco": 69.90,
+    "capa": "https://covers.openlibrary.org/b/isbn/9780132350884-L.jpg"
+  },
+  {
+    "id": 3,
+    "titulo": "Harry Potter e o Prisioneiro de Azkaban",
+    "autor": "J.K. Rowling",
+    "resenha": "Um livro sobre boas práticas de programação, escrita de código limpo e melhoria da qualidade de software.",
+    "preco": 69.90,
+    "capa": "https://covers.openlibrary.org/b/isbn/9780132350884-L.jpg"
+  },
+  {
+    "id": 4,
+    "titulo": "Harry Potter e o Cálice de Fogo",
+    "autor": "J.K. Rowling",
+    "resenha": "Um livro sobre boas práticas de programação, escrita de código limpo e melhoria da qualidade de software.",
+    "preco": 69.90,
+    "capa": "https://covers.openlibrary.org/b/isbn/9780132350884*/-L.jpg"
+  },
+  {
+    "id": 5,
+    "titulo": "O Príncipe Cruel",
+    "autor": "Holly Black",
+    "resenha": "Livro clássico sobre padrões de projeto e soluções reutilizáveis para problemas comuns de software.",
+    "preco": 59.90,
+    "capa": "https://covers.openlibrary.org/b/isbn/9780201633610-L.jpg"
+  },
+  {
+    "id": 6,
+    "titulo": "O Rei Perverso",
+    "autor": "Holly Black",
+    "resenha": "Referência ampla sobre algoritmos, estruturas de dados e análise de complexidade.",
+    "preco": 59.90,
+    "capa": "https://covers.openlibrary.org/b/isbn/9780262033848-L.jpg"
+  },
+  {
+    "id": 7,
+    "titulo": "A Rainha do Nada",
+    "autor": "Holly Black",
+    "resenha": "Apresenta técnicas para melhorar a estrutura interna do código sem alterar seu comportamento externo.",
+    "preco": 59.90,
+    "capa": "https://covers.openlibrary.org/b/isbn/9780134757599-L.jpg"
+  },
+  {
+    "id": 8,
+    "titulo": "O Canto Mais Escuro da Floresta",
+    "autor": "Holly Black",
+    "resenha": "Livro introdutório e visual sobre construção de páginas web com HTML e CSS.",
+    "preco": 59.90,
+    "capa": "https://covers.openlibrary.org/b/isbn/9781118008188-L.jpg"
+  }
+]
+
+export default produtos;
