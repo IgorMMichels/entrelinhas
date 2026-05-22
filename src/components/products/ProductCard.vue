f62cdfd1be2583ddef69b3786a7c41a0ac98d9fe
IgorMMichels <igor.marcon.michels@gmail.com>
2026-05-22 09:35:05 -0300

    feat: adicionar dados de produtos e componentes de listagem

---
+++ src/components/products/ProductCard.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { Card } from '@/components/ui/card'
+import Button from '../ui/button/Button.vue'
+defineProps(['id', 'autor', 'titulo', 'resenha', 'preco', 'capa'])
+import { HugeiconsIcon } from '@hugeicons/vue'
+import { Heart } from '@hugeicons/core-free-icons'
+import { formataPreco } from '@/utils/currencyUtils'
+</script>
+
+<template>
+  <Card class="grid grow w-full max-w-sm m-0 p-0 overflow-hidden">
+    <img
+      :src="capa"
+      alt=""
+      class="w-full h-100 object-cover rounded-t-md transition-transform duration-300 hover:scale-105"
+    />
+    <div class="w-full p-5 pt-0">
+      <h1>{{ titulo }}</h1>
+      <div class="w-full flex justify-between">
+        <h4> {{ autor }}</h4>
+        <HugeiconsIcon :icon="Heart" color="#4b0001" style="cursor: pointer" />
+      </div>
+      <h4 class='price'>{{ formataPreco(preco) }}</h4>
+      <h3> {{ resenha }} </h3>
+      <Button variant="outline" class="w-full"> Adicionar ao carrinho </Button>
+    </div>
+  </Card>
+</template>
+
+<style scoped>
+button {
+  background-color: #588157;
+  color: var(--bg-color);
+  cursor: pointer;
+  transition: 0.3s all ease;
+  margin-bottom: 0;
+}
+
+button:hover {
+  transform: translateY(-3px);
+  box-shadow: rgba(173, 255, 172, 0.8) 0px 5px 15px;
+}
+
+h1 {
+  font-size: 22px;
+  color: var(--font-color);
+}
+
+h3 {
+  font-size: 14px;
+  margin: 1rem 0;
+  width: 90%;
+}
+
+.price {
+ font-size: 22px;
+}
+</style>
---
+++ src/components/products/ProductList.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import ProductCard from './ProductCard.vue';
+  import produtos from '@/data/products';
+  const listaProdutos = ref(produtos)
+</script>
+
+<template>
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <ProductCard
+    v-for="produto in listaProdutos" :key="produto.id - 1"
+    :capa="produto.capa"
+    :titulo="produto.titulo"
+    :preco="produto.preco"
+    :autor="produto.autor"
+    :resenha="produto.resenha"
+    ></ProductCard>
+  </div>
+</template>
+
+<style scoped>
+</style>
