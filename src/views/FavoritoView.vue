63339052f0c31f1ed70c83e05501455ca4634758
IgorMMichels <igor.marcon.michels@gmail.com>
2026-06-02 09:23:13 -0300

    feat: favoritoView Page

---
+++ src/views/FavoritoView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import FavoritoList from '@/components/favoritos/FavoritoList.vue';
+import AppFooter from '@/components/layout/appFooter.vue';
+import AppHeader from '@/components/layout/appHeader.vue';
+
+</script>
+
+<template>
+<AppHeader></AppHeader>
+<FavoritoList></FavoritoList>
+<AppFooter></AppFooter>
+</template>
